716624dd7e435618e8dad3e3ae6dcaffba613011
Stephanie Cheney <steph@scheneyphotography.com>
2020-04-25 08:15:25 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color,:height, 
+  :weight, :handed, :complexion, :t_shirt_size, 
+  :wrist_size, :glove_size, :pant_length, :pant_width
+  
+hash = {name: "Stephanie", birthday: "July23", hair_color: "brown", eye_color: "brown", height: 67, 
+weight: 125, handed: "right", complexion: "fair", t_shirt_size:"small", 
+wrist_size: "small", glove_size: 3, pant_length: 5, pant_width: 6}
+
+def initialize(hash)
+  hash.each do |key, value| self.send(("#{key}="), value) end 
+end 
 end
